d74e43a80cf1da5d2d2d5a426480587eaeae5b3d
Tom Ogasawara <tom.ogasawara@gmail.com>
2017-03-04 11:31:14 -0600

    Write function to extract sequence

--- src/App.js
+++ src/App.js
@@ -2,10 +2,34 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      sequence: "",
+      sequenceLength: 0,
+      features: []
+    };
+  }
+
+  search() {
+    const URL = "https://teselagen.com/getSequence";
+
+    fetch(URL, {
+      method: 'GET'
+    })
+    .then(response => response.json())
+    .then(json1 => {
+      // console.log(json1);
+      const sequence = json1.sequence;
+      this.setState({sequence});
+    });
+  }
+
   render() {
     return (
       <div className="App">
-
+        {this.search()}
       </div>
     );
   }
