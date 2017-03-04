9299f2c22bb035841c79799f7366ef98722668d2
Tom Ogasawara <tom.ogasawara@gmail.com>
2017-03-04 11:43:29 -0600

    Add react-bootstrap

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      sequence: "",
+      sequenceLength: 0,
+      features: []
+    };
+  }
+
+  search() {
+    const URL = "https://teselagen.com/getSequence";
+
+    fetch(URL, {
+      method: 'GET'
+    })
+    .then(response => response.json())
+    .then(json => {
+      const sequence = json.sequence;
+      const sequenceLength = json.sequenceLength;
+      const features = json.features;
+
+      this.setState({sequence});
+      this.setState({sequenceLength});
+      this.setState({features});
+      // console.log(this.state);
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-title">Plasmid Visualization</div>
+        <FormGroup className='search-button'>
+          <InputGroup>
+            <InputGroup.Addon onClick={() => this.search()}>
+              <Glyphicon glyph="search"></Glyphicon>
+            </InputGroup.Addon>
+          </InputGroup>
+        </FormGroup>
+        {
+          this.state.sequence !== null
+          ?
+            <div>
+              {this.state.sequence}
+            </div>
+          : <div></div>
+        }
+
+      </div>
+    );
+  }
+}
+
+export default App;
